1f42438a2216de00442bbb353f9f4bb8cfdfbf89
Christopher Kim <cj3kim@gmail.com>
2017-02-12 16:31:47 -0800

    Super charged test cases by making them more functional.

--- tests/metaLine.js
+++ tests/metaLine.js
@@ -1,28 +1,37 @@
-
 var assert = require('assert');
 var metaLine = require('../lib/metaLine');
 
+function runTest (fn, input, expectedResult) {
+  var result = fn.apply(null, input)
+  var templateString = "input: " + input + ", " + "expected:  " + JSON.stringify(expectedResult);
+  it(templateString, function () { assert.deepEqual(result, expectedResult) })
+}
+function runTests(fn, inputs, expectedResults) {
+    inputs.forEach(function(e, i) { runTest(fn, e, expectedResults[i]); })
+}
+
 describe("metaLine", function () {
+    var fn = metaLine;
+    var inputs = [
+      ["describe decorateRelatedData"],
+      ["  context when one should dance"],
+      ["    this should be level 2"]
+    ];
 
-  it("should generate the correct obj", function () {
-    var line   = "describe decorateRelatedData";
-    var result = metaLine(line);
-
-    assert.deepEqual(result, {
-      level: 0,
-      fn: "describe",
-      title: "decorateRelatedData",
-    });
-
-    var line   = "  context when one should dance";
-    var result = metaLine(line);
-
-    assert.deepEqual(result, {
-      level: 1,
-      fn: "context",
-      title: "when one should dance",
-    });
-
-  });
+    var expectedResults = [
+        { level: 0,
+          fn: "describe",
+          title: "decorateRelatedData",
+        },
+        { level: 1,
+          fn: "context",
+          title: "when one should dance",
+        },
+        { level: 2,
+          fn: "this",
+          title: "should be level 2",
+        }
+    ]
+    runTests(metaLine, inputs, expectedResults)
 
 });
